722b7d5a5c049373a4744d3378e178ef0a0074a3
Snehal Jadhav <145901611+snehalrj216@users.noreply.github.com>
2026-02-18 20:16:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+// Select the target element
+let element = document.getElementById("level");
+
+let count = 0;
+
+// Traverse up the DOM tree
+while (element) {
+    count++;
+    element = element.parentElement;
+}
+
+// Show result
+alert("The level of the element is: " + count);
